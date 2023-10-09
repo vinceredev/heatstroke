05c09b5a4806bd8ff1ef7a5865a18a648be1b633
Alfred Crosby <alfred@vinceredigital.com>
2023-10-09 23:41:04 +0700

    add missing commands

--- src/Console/InstallCommand.php
+++ src/Console/InstallCommand.php
@@ -4,6 +4,8 @@ namespace Vincere\Heatstroke\Console;
 
 use Illuminate\Console\Command;
 use Illuminate\Contracts\Console\PromptsForMissingInput;
+use RuntimeException;
+use Symfony\Component\Process\Process;
 
 class InstallCommand extends Command implements PromptsForMissingInput
 {
@@ -14,6 +16,61 @@ class InstallCommand extends Command implements PromptsForMissingInput
     public function handle()
     {
         $this->installHeatstrokeStack();
+    }
+
+    protected function requireComposerPackages(array $packages, $asDev = false)
+    {
+        $command = array_merge(
+            $command ?? ['composer', 'require'],
+            $packages,
+            $asDev ? ['--dev'] : [],
+        );
+
+        return (new Process($command, base_path(), ['COMPOSER_MEMORY_LIMIT' => '-1']))
+        ->setTimeout(null)
+            ->run(function ($type, $output) {
+                $this->output->write($output);
+            }) === 0;
+    }
+
+    protected static function updateNodePackages(callable $callback, $dev = true)
+    {
+        if (!file_exists(base_path('package.json'))) {
+            return;
+        }
+
+        $configurationKey = $dev ? 'devDependencies' : 'dependencies';
+
+        $packages = json_decode(file_get_contents(base_path('package.json')), true);
+
+        $packages[$configurationKey] = $callback(
+            array_key_exists($configurationKey, $packages) ? $packages[$configurationKey] : [],
+            $configurationKey
+        );
+
+        ksort($packages[$configurationKey]);
+
+        file_put_contents(
+            base_path('package.json'),
+            json_encode($packages, JSON_UNESCAPED_SLASHES | JSON_PRETTY_PRINT) . PHP_EOL
+        );
+    }
+
+    protected function runCommands($commands)
+    {
+        $process = Process::fromShellCommandline(implode(' && ', $commands), null, null, null, null);
+
+        if ('\\' !== DIRECTORY_SEPARATOR && file_exists('/dev/tty') && is_readable('/dev/tty')) {
+            try {
+                $process->setTty(true);
+            } catch (RuntimeException $e) {
+                $this->output->writeln('  <bg=yellow;fg=black> WARN </> ' . $e->getMessage() . PHP_EOL);
+            }
+        }
+
+        $process->run(function ($type, $line) {
+            $this->output->write('    ' . $line);
+        });
     }
 
     public function installHeatstrokeStack()
